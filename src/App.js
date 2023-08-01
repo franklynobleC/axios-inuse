7baf44e0c374f750e5f8bc62d543a51ca2ac20cd
franklynobleC <franklyessien01@gmail.com>
2023-08-01 02:57:13 +0100

    axios using get Method

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Title from './components/Title'
+import Setup from './examples/2-headers'
+function App () {
+  return (
+    <main>
+      <Setup />
+
+      <Title />
+    </main>
+  )
+}
+
+export default App
